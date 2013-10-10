6f5cb7368a087aa3ee1cc3fa54f04b70f1af4145
Jared Lobberecht <pretender314@gmail.com>
2013-10-10 08:54:05 -0500

    removing dev deploy repo

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-source "0.1.0-SNAPSHOT"
   :description "A Leiningen plugin to pull project configuration from different locations."
+  :url "https://github.com/Jared314/lein-source"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true
-  :deploy-repositories [["testplugin" "file:///Users/jared314/Desktop/source-test/repo"]])
+  :eval-in-leiningen true)
