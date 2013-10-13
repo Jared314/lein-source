1508de2794d9f1bbf7e33446d512d3bffe571f44
Jared Lobberecht <pretender314@gmail.com>
2013-10-13 02:10:00 -0500

    bumping snapshot version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-source "0.1.1-SNAPSHOT"
+(defproject lein-source "0.2.0-SNAPSHOT"
   :description "A Leiningen plugin to pull project configuration from different locations."
-  :url "https://github.com/Jared314/lein-source/tree/v0.1.1"
+  :url "https://github.com/Jared314/lein-source/tree/v0.2.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-jgit "0.5.3"]]
