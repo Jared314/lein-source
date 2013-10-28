a646424d37caf0f25c580367d4e02d1d599f147f
Jared Lobberecht <pretender314@gmail.com>
2013-10-28 00:40:32 -0500

    switching to new analyzer and writer

--- src/leiningen/base.clj
+++ src/leiningen/base.clj
@@ -1,4 +1,4 @@
-(ns leiningen.store
+(ns leiningen.base
   (:require [clojure.string :as string]
             [clojure.java.io :as io]
             [clojure.walk :as walk]
@@ -10,6 +10,10 @@
   (:import [org.eclipse.jgit.treewalk TreeWalk]
            [java.net URL Proxy URLStreamHandlerFactory URLStreamHandler URLConnection]
            [java.io File PushbackReader]))
+
+
+(defn update-values [f m]
+  (into {} (for [[k v] m] [k (f k v)])))
 
 ;;;;;;;;;;;;;;;;;;;
 ;; Form Reading
@@ -31,101 +35,76 @@
         defing? (and current-ns
                      (symbol? op)
                      (.startsWith (name op) "def"))
+        nsname (str (if ns? (second x) current-ns))
         naming (let [nsym (second x)]
                  (cond
                   ns? (str nsym)
                   defing? (if (namespace nsym)
                             (str nsym)
                             (str (symbol (name current-ns) (name nsym))))))]
-    (cond-> {:clj/op (str op)
+    (cond-> {:clj/ns nsname
+             :clj/op (str op)
              :clj/form x}
-            ns? (assoc :clj/ns naming)
             defing? (assoc :clj/def naming))))
 
 (defn analyze [src]
   (let [ns? #(and (coll? %) (= 'ns (first %)))]
-    (with-open [r (PushbackReader. (StringReader. src))]
-      (second (doall
-               (reduce #(let [[current-ns data] %1
-                              current-ns (if (ns? %2) (second %2) current-ns)]
-                          [current-ns
-                           (conj data (analyze-form current-ns %2))])
-                       [nil []]
-                       (lazy-read r)))))))
+  (with-open [r (PushbackReader. (io/reader src))]
+    (second (doall
+             (reduce #(let [[current-ns data] %1
+                            current-ns (if (ns? %2) (second %2) current-ns)]
+                        [current-ns
+                         (conj data (analyze-form current-ns %2))])
+                     [nil []]
+                     (lazy-read r)))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; File-based "DB"
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defrecord Leaf [nss declares forms])
+(defn generate-declare-form [nsname data]
+  (let [defed-syms (sort (map #(-> % :clj/def symbol name)
+                              (filter :clj/def data)))]
+    {:clj/ns nsname
+     :clj/op "declare"
+     :clj/def defed-syms
+     :clj/form (conj (map symbol defed-syms) 'declare)}))
 
-(def default-layout {"src" {}
-                     "test" {}
-                     "project" (map->Leaf {:forms ["(defproject tester1 \"0.1.0-SNAPSHOT\" :dependencies [[org.clojure/clojure \"1.5.1\"]])"]})})
+(defn sort-forms [coll]
+  (sort-by #(case (:clj/op %) "ns" "0" "declare" "1" (:clj/def %)) coll))
 
-(defn parse-value [value]
-  value)
+(defn format-forms [current-ns item]
+  (sort-forms (conj item (generate-declare-form current-ns item))))
 
-(defn compare-value [v1 v2]
-  (compare (parse-value v1) (parse-value v2)))
+(defn format-namespaces [data]
+  (update-values (fn [k v]
+                   (clojure.string/join "\n\n" (map :clj/form (format-forms k v))))
+                 data))
 
-(defn insert! [data item]
-  (if (nil? data)
-    (Leaf. [(:ns item)] [(:name item)] (sorted-set-by compare-value (:source item)))
-    (let [nss (conj (:nss data) (:ns item))
-          declares (conj (:declares data) (:name item))
-          forms (conj (:forms data) (:source item))]
-      (assoc data :nss nss :declares declares :forms forms))))
+(defn generate-writes [basepath [k v]]
+  (let [p (-> k namespace-munge (clojure.string/replace \. \/) (str ".clj"))
+        f (io/file basepath p)]
+    [#(.mkdirs (.getParentFile f))
+     #(spit f v)]))
 
-(defn parse-ns-name [value]
-  (string/split value #"\."))
-
-(defn parse [base key namespace value]
-  (update-in base
-             (concat [key] (parse-ns-name namespace))
-             #(insert! % value)))
-
-(defn layout-data [data]
-  (if (not (instance? Leaf data))
-    (list data)
-    (lazy-cat (map #(:source %) (:nss data)) ; ns
-              (map #(str "(declare " % ")") (:declares data)) ; declare
-              (:forms data))))
-
-(defn format-name [value] (string/replace value "-" "_"))
-
-(defn write! [base-path data]
-  (let [keys (keys data)
-        leaves (filter #(instance? Leaf (data %)) keys)
-        nodes (filter #(not (instance? Leaf (data %))) keys)
-        base-file (io/as-file base-path)]
-    (dorun (map #(spit (str base-path File/separator (format-name %) ".clj")
-                       (string/join "\n" (layout-data (data %))))
-                leaves))
-    (for [n nodes]
-      (let [new-path (str base-path File/separator (format-name n))
-            new-file (io/as-file new-path)]
-        (when-not (.exists new-file) (.mkdir new-file))
-        (write! new-path (data n))))))
+(defn generate-ns-writes [basepath m]
+  (->> m
+       (group-by :clj/ns)
+       format-namespaces
+       (reduce #(into %1 (generate-writes basepath %2)) [])))
 
 
-
-(def result
-     (reduce #(parse %1 "src" (:name (:ns %2)) %2)
-             default-layout
-             data))
-
-
-;; echo "(ns stuff) (defn thing [] true)" | lein store --stdin
-
-;(def data
-;  (java.io.StringReader. "(ns stuff) (defn thing [] true)"))
-
-
+;;
+;; echo "(ns stuff.core) (defn thing [] true)" | lein base .
+;;
 (defn
   base
-  "A Leiningen plugin to store forms to a backend."
+  "A Leiningen plugin to read, write, and run forms in a backend."
   [project & args]
-  (let [targetpath (io/as-file (first args))]
-    (read-all *in*)
-    ))
+  (let [targetpath (.getCanonicalFile (io/as-file (first args)))]
+    (->> *in*
+         analyze
+         (generate-ns-writes (io/file targetpath "src"))
+         (map #(%))
+         dorun)
+    (println "Done.")))
